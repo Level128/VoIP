0078cfd4ceede68bb96c5ae2cc07d006c1df8a89
Level128 <30358400+Level128@users.noreply.github.com>
2023-07-13 11:31:28 -0500

    Update Refresh.vue to Vue3 format

---
+++ frontend/src/components/RefreshItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import PullToRefresh from 'pulltorefreshjs'
+import { onMounted } from 'vue'
+
+onMounted(() => PullToRefresh.init({
+  mainElement: 'body',
+  onRefresh () {
+    window.location.reload()
+  },
+  distThreshold: 120,
+  distMax: 140
+}))
+
+</script>
