8d36056ba9c74803fe83c678f0d1fa7aea92e733
Level128 <30358400+Level128@users.noreply.github.com>
2023-07-13 11:28:02 -0500

    Convert App.vue to Vue3 format

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,55 +1,34 @@
 <template>
   <div id="app">
-    <span v-if="oldVersion" class="update_ribbon"
-      ><a
-        href="https://github.com/0perationPrivacy/VoIP/blob/main/CHANGELOG.md"
-        target="_blank"
-        rel="noopener noreferrer"
-        >update</a
-      ></span
-    >
+    <span v-if="oldVersion" class="update_ribbon"><a
+        href="https://github.com/0perationPrivacy/VoIP/blob/main/CHANGELOG.md" target="_blank"
+        rel="noopener noreferrer">update</a></span>
     <RouterView />
   </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
 import { get } from './core/module/common.module'
+import { useStore } from 'vuex'
 
-const oldVersion
+const store = useStore()
+const oldVersion = ref(false)
 
-export default {
-  name: 'App',
-  data () {
-    return {
-      oldVersion: false,
-      dir: ''
-    }
-  },
-  mounted () {
-    this.getVersion()
-    // this.checkDirectoryName()
-  },
-  methods: {
-    getVersion () {
-      const request = {
-        url: 'auth/get-update-version'
-      }
-      this.$store
-        .dispatch(get, request)
-        .then((response) => {
-          // console.log(response.update)
-          if (response.update === 'true') {
-            this.oldVersion = true
-          } else {
-            this.oldVersion = false
-          }
-        })
-        .catch((e) => {
-          this.oldVersion = false
-          console.log(e)
-          // resolve(false)
-        })
-    }
+onMounted(() => getVersion())
+
+function getVersion () {
+  const request = {
+    url: 'auth/get-update-version'
   }
+  store
+    .dispatch(get, request)
+    .then((response) => {
+      oldVersion.value = response.update === 'true'
+    })
+    .catch((e) => {
+      oldVersion.value = false
+      console.error(e)
+    })
 }
 </script>
