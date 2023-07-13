4fc1b792b0743dacc54bf0e4535d201511cbe642
Level128 <30358400+Level128@users.noreply.github.com>
2023-07-13 11:19:21 -0500

    Covert CheckDir.vue to Vue3 format

--- frontend/src/components/CheckDir.vue
+++ frontend/src/components/CheckDir.vue
@@ -1,52 +1,38 @@
 <template>
-    <div></div>
+  <div></div>
 </template>
 
-<script>
+<script setup>
+import { onMounted } from 'vue'
 import { post } from '../core/module/common.module'
-export default {
-  mounted () {
-    this.checkDirectoryName()
-  },
-  methods: {
-    checkDirectoryName () {
-      // console.log('check params')
-      // console.log(this.$route.name)
-      // console.log(this.$route.params)
-      // setTimeout(() => {
-      var request = {
-        url: 'auth/check-directoryname',
-        data: {dirname: this.$route.params.appdirectory}
-      }
-      this.$store
-        .dispatch(post, request)
-        .then((response) => {
-          console.log('get dir')
-          console.log(response)
-          if (response.data.status === 'nodir' || response.data.status === 'no-name') {
-            // this.$router.push(`/${this.$route.params.appdirectory}/dashboard`)
-            this.$router.push(`/voip/dashboard`)
-            // if (this.$route.params.appdirectory === undefined) {
-            //   this.$router.push(`/voip/`)
-            // }
-            // else {
-            //   if (this.$route.params.appdirectory !== 'voip') {
-            //     this.$router.push(`/404`)
-            //   }
-            // }
-          } else if (response.data.status === 'false') {
-            // console.log('404 route')
-            // console.log(response.data.status)
-            this.$router.push(`/404`)
-          }
-        })
-        .catch((e) => {
-          this.old_version = false
-          console.log(e)
-          // resolve(false)
-        })
-      // }, 500)
-    }
+import { useRoute, useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+
+const router = useRouter()
+const route = useRoute()
+const store = useStore()
+
+onMounted(() => checkDirectoryName())
+
+function checkDirectoryName () {
+  const request = {
+    url: 'auth/check-directoryname',
+    data: { dirname: route.params.appdirectory }
   }
+  store
+    .dispatch(post, request)
+    .then((response) => {
+      // check against the backend to see if we are using the correct APPDIRECTORY
+      // this is applied client-side, so could this be evaded manually changing it?
+      if (response.data.status === 'false') {
+        router.push('/404')
+      } else {
+        router.push(`/${route.params.appdirectory}/dashboard`)
+      }
+    })
+    .catch((e) => {
+      this.old_version = false
+      console.error(e)
+    })
 }
 </script>
